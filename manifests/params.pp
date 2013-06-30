0b4af772678796678f6e7d53bf8e45da19e44325
Wolf Noble <wolf@wolfspyre.com>
2013-06-30 16:32:53 -0500

    minor changes to params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,23 +1,23 @@
 class mongodb::params{
   case $::osfamily {
-    'redhat': {
+    'RedHat': {
       case $::architecture {
         /i386|i686/: {
           $baseurl = 'http://downloads-distro.mongodb.org/repo/redhat/os/i686'
         }
-        'x86_64':{
+        'x86_64': {
           $baseurl = 'http://downloads-distro.mongodb.org/repo/redhat/os/x86_64'
         }
-        default:{
+        default: {
           $baseurl = 'http://downloads-distro.mongodb.org/repo/redhat/os/x86_64'
         }
       }
-      $source  = 'mongodb::sources::yum'
-      $package = 'mongodb-server'
-      $service = 'mongod'
+      $source    = 'mongodb::sources::yum'
+      $package   = 'mongodb-server'
+      $service   = 'mongod'
       $pkg_10gen = 'mongo-10gen-server'
     }
-    'debian': {
+    'Debian': {
       $locations = {
         'sysv'    => 'http://downloads-distro.mongodb.org/repo/debian-sysvinit',
         'upstart' => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart'
@@ -26,9 +26,9 @@ class mongodb::params{
         'Debian': { $init = 'sysv' }
         'Ubuntu': { $init = 'upstart' }
       }
-      $source  = 'mongodb::sources::apt'
-      $package = 'mongodb'
-      $service = 'mongodb'
+      $source    = 'mongodb::sources::apt'
+      $package   = 'mongodb'
+      $service   = 'mongodb'
       $pkg_10gen = 'mongodb-10gen'
     }
     default: {
